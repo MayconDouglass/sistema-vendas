04081ab4dd31d7954435d2abd267b1e39261ce75
Maycon <dqsmaycon2@gmail.com>
2020-02-26 00:38:41 -0300

    Remoção do último teste.

--- src/DAO/DAOCidades.java
+++ src/DAO/DAOCidades.java
@@ -3,103 +3,107 @@ package DAO;
 import model.ModelCidades;
 import Connect.ConexaoMySql;
 import java.util.ArrayList;
+
 /**
-*
-* @author Maycon Douglas
-*/
+ *
+ * @author Maycon Douglas
+ */
 public class DAOCidades extends ConexaoMySql {
 
     /**
-    * grava Cidades
-    * @param pModelCidades
-    * @return int
-    */
-    public int salvarCidadesDAO(ModelCidades pModelCidades){
+     * grava Cidades
+     *
+     * @param pModelCidades
+     * @return int
+     */
+    public int salvarCidadesDAO(ModelCidades pModelCidades) {
         try {
             this.conectar();
             return this.insertSQL(
-                "INSERT INTO cidades ("
+                    "INSERT INTO cidades ("
                     + "id_cidade,"
                     + "descricao,"
                     + "uf,"
                     + "codigo_ibge,"
                     + "ddd"
-                + ") VALUES ("
+                    + ") VALUES ("
                     + "'" + pModelCidades.getIdCidade() + "',"
                     + "'" + pModelCidades.getDescricao() + "',"
                     + "'" + pModelCidades.getUf() + "',"
                     + "'" + pModelCidades.getCodigoIbge() + "',"
                     + "'" + pModelCidades.getDdd() + "'"
-                + ");"
+                    + ");"
             );
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return 0;
-        }finally{
+        } finally {
             this.fecharConexao();
         }
     }
 
     /**
-    * recupera Cidades
-    * @param pIdCidade
-    * @return ModelCidades
-    */
-    public ModelCidades getCidadesDAO(int pIdCidade){
+     * recupera Cidades
+     *
+     * @param pIdCidade
+     * @return ModelCidades
+     */
+    public ModelCidades getCidadesDAO(int pIdCidade) {
         ModelCidades modelCidades = new ModelCidades();
         try {
             this.conectar();
             this.executarSQL(
-                "SELECT "
+                    "SELECT "
                     + "id_cidade,"
                     + "descricao,"
                     + "uf,"
                     + "codigo_ibge,"
                     + "ddd"
-                 + " FROM"
-                     + " cidades"
-                 + " WHERE"
-                     + " id_cidade = '" + pIdCidade + "'"
-                + ";"
+                    + " FROM"
+                    + " cidades"
+                    + " WHERE"
+                    + " id_cidade = '" + pIdCidade + "'"
+                    + ";"
             );
 
-            while(this.getResultSet().next()){
+            while (this.getResultSet().next()) {
                 modelCidades.setIdCidade(this.getResultSet().getInt(1));
                 modelCidades.setDescricao(this.getResultSet().getString(2));
                 modelCidades.setUf(this.getResultSet().getString(3));
                 modelCidades.setCodigoIbge(this.getResultSet().getInt(4));
                 modelCidades.setDdd(this.getResultSet().getString(5));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             this.fecharConexao();
         }
         return modelCidades;
     }
 
     /**
-    * recupera uma lista de Cidades
-        * @return ArrayList
-    */
-    public ArrayList<ModelCidades> getListaCidadesDAO(){
+     * recupera uma lista de Cidades
+     *
+     * @return ArrayList
+     */
+    public ArrayList<ModelCidades> getListaCidadesDAO() {
         ArrayList<ModelCidades> listamodelCidades = new ArrayList();
         ModelCidades modelCidades = new ModelCidades();
         try {
             this.conectar();
             this.executarSQL(
-                "SELECT "
+                    "SELECT "
                     + "id_cidade,"
                     + "descricao,"
                     + "uf,"
                     + "codigo_ibge,"
                     + "ddd"
-                 + " FROM"
-                     + " cidades"
-                + ";"
+                    + " FROM"
+                    + " cidades"
+                    + ";"
             );
 
-            while(this.getResultSet().next()){
+            while (this.getResultSet().next()) {
                 modelCidades = new ModelCidades();
                 modelCidades.setIdCidade(this.getResultSet().getInt(1));
                 modelCidades.setDescricao(this.getResultSet().getString(2));
@@ -108,59 +112,101 @@ public class DAOCidades extends ConexaoMySql {
                 modelCidades.setDdd(this.getResultSet().getString(5));
                 listamodelCidades.add(modelCidades);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             this.fecharConexao();
         }
         return listamodelCidades;
     }
 
     /**
-    * atualiza Cidades
-    * @param pModelCidades
-    * @return boolean
-    */
-    public boolean atualizarCidadesDAO(ModelCidades pModelCidades){
+     * recupera uma lista de Cidades do UF selecionado
+     *
+     * @return ArrayList
+     */
+    public ArrayList<ModelCidades> getListaCidadesUFDAO(String pUF) {
+        ArrayList<ModelCidades> listamodelCidades = new ArrayList();
+        ModelCidades modelCidades = new ModelCidades();
+        try {
+            this.conectar();
+            this.executarSQL(
+                    "SELECT "
+                    + "id_cidade,"
+                    + "descricao,"
+                    + "uf,"
+                    + "codigo_ibge,"
+                    + "ddd"
+                    + " FROM"
+                    + " cidades"
+                    + " WHERE uf = '" + pUF + "'"
+                    + ";"
+            );
+
+            while (this.getResultSet().next()) {
+                modelCidades = new ModelCidades();
+                modelCidades.setIdCidade(this.getResultSet().getInt(1));
+                modelCidades.setDescricao(this.getResultSet().getString(2));
+                modelCidades.setUf(this.getResultSet().getString(3));
+                modelCidades.setCodigoIbge(this.getResultSet().getInt(4));
+                modelCidades.setDdd(this.getResultSet().getString(5));
+                listamodelCidades.add(modelCidades);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.fecharConexao();
+        }
+        return listamodelCidades;
+    }
+
+    /**
+     * atualiza Cidades
+     *
+     * @param pModelCidades
+     * @return boolean
+     */
+    public boolean atualizarCidadesDAO(ModelCidades pModelCidades) {
         try {
             this.conectar();
             return this.executarUpdateDeleteSQL(
-                "UPDATE cidades SET "
+                    "UPDATE cidades SET "
                     + "id_cidade = '" + pModelCidades.getIdCidade() + "',"
                     + "descricao = '" + pModelCidades.getDescricao() + "',"
                     + "uf = '" + pModelCidades.getUf() + "',"
                     + "codigo_ibge = '" + pModelCidades.getCodigoIbge() + "',"
                     + "ddd = '" + pModelCidades.getDdd() + "'"
-                + " WHERE "
+                    + " WHERE "
                     + "id_cidade = '" + pModelCidades.getIdCidade() + "'"
-                + ";"
+                    + ";"
             );
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
-        }finally{
+        } finally {
             this.fecharConexao();
         }
     }
 
     /**
-    * exclui Cidades
-    * @param pIdCidade
-    * @return boolean
-    */
-    public boolean excluirCidadesDAO(int pIdCidade){
+     * exclui Cidades
+     *
+     * @param pIdCidade
+     * @return boolean
+     */
+    public boolean excluirCidadesDAO(int pIdCidade) {
         try {
             this.conectar();
             return this.executarUpdateDeleteSQL(
-                "DELETE FROM cidades "
-                + " WHERE "
+                    "DELETE FROM cidades "
+                    + " WHERE "
                     + "id_cidade = '" + pIdCidade + "'"
-                + ";"
+                    + ";"
             );
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
-        }finally{
+        } finally {
             this.fecharConexao();
         }
     }
